bf5585db4a85855eb1e0a7fa634befad1ce1a77d
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-04 13:36:43 +0800

    make watcher use a vision task queue

--- face/watcher.py
+++ face/watcher.py
@@ -2,17 +2,17 @@ import json
 from threading import Thread, Event
 
 from .tracker import Tracker
+from .vision import VisionTask
 from .utils.logger import init_logger
-from .recognizer import FaceRecognizer
 from .video import VideoDeviceSettings, VideoStream
 
 
 class FaceWatcher(Thread):
 
-    def __init__(self, video_conf, log=None):
+    def __init__(self, task_queue, video_conf, log=None):
+        self.task_queue = task_queue
         self.log = log or init_logger('faceid')
         self.stream = self.video_stream(video_conf)
-        self.recognizer = FaceRecognizer(self.log)
         self.tracker = Tracker(self.stream.size)
         self.join_event = Event()
         super().__init__()
@@ -36,6 +36,7 @@ class FaceWatcher(Thread):
         return stream
 
     def run(self):
+        task = VisionTask()
 
         while not self.join_event.is_set():
             frame = self.stream.read()
@@ -43,6 +44,11 @@ class FaceWatcher(Thread):
             if frame is None:
                 self.log.warning(f'Failed to read from {self.stream.path}')
                 continue
+
+            task.image = frame
+            self.task_queue.put(task)
+            faces = task.faces
+            self.log.info('Detected {len(faces)} faces')
 
     def join(self, timeout=None):
         self.join_event.set()
