c50b90ffb1487bf7912706bb1b54790eb819c5a4
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-04 15:43:48 +0800

    add show_targets method to face watcher

--- face/watcher.py
+++ face/watcher.py
@@ -1,7 +1,6 @@
 import json
 from threading import Thread, Event
 
-from .tracker import Tracker
 from .vision import VisionTask
 from .utils.logger import init_logger
 from .video import VideoDeviceSettings, VideoStream
@@ -9,13 +8,13 @@ from .video import VideoDeviceSettings, VideoStream
 
 class FaceWatcher(Thread):
 
-    def __init__(self, task_queue, video_conf, log=None):
+    def __init__(self, task_queue, video_conf, show=False, log=None):
         self.task_queue = task_queue
         self.log = log or init_logger('faceid')
         self.stream = self.video_stream(video_conf)
-        self.tracker = Tracker(self.stream.size)
+        self.show = show
         self.join_event = Event()
-        super().__init__()
+        super().__init__(name='FaceWatcher')
 
     def video_stream(self, conf_file):
 
@@ -36,6 +35,7 @@ class FaceWatcher(Thread):
         return stream
 
     def run(self):
+        self.log.info('Start watching faces...')
         task = VisionTask()
 
         while not self.join_event.is_set():
@@ -48,9 +48,27 @@ class FaceWatcher(Thread):
             task.image = frame
             self.task_queue.put(task)
             faces = task.faces
-            self.log.info('Detected {len(faces)} faces')
+            self.log.info(f'Detected {len(faces)} faces')
+
+            if self.show:
+                # TODO: track face targets
+                targets = []
+                self.show_targets(frame, targets)
+
+    def show_targets(self, frame, targets):
+
+        for t in targets:
+            anchor = (t.bbox[0], t.bbox[1] - 5)
+            VideoStream.draw_text(frame, t.label, anchor)
+            VideoStream.draw_box(frame, t.bbox)
+
+        VideoStream.show(frame, title=self.stream.path)
+
+        if VideoStream.is_key_pressed('q'):
+            self.join_event.set()
 
     def join(self, timeout=None):
+        self.log.info(f'Stopping {self.name} thread...')
         self.join_event.set()
         super().join(timeout)
 
