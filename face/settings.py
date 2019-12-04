56b1514e53bd5e07b245adfe41bba6cc72553290
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-04 11:33:46 +0800

    add video stream json settings

--- face/settings.py
+++ face/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 root = Path(__file__).parent.joinpath('cfg')
 model_conf_file = root.joinpath('models.json')
+video_conf_files = root.joinpath('video').glob('*.json')
 
 clf_model_params = {
     'estimator__gamma': 'auto',
