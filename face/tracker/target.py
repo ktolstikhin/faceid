6e6ce1a9c777a4bd299566f9801ea86abc8c1f00
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-06 11:47:08 +0800

    make face target thread safe

--- face/tracker/target.py
+++ face/tracker/target.py
@@ -1,4 +1,5 @@
 import uuid
+from threading import Lock
 
 
 class FaceTarget:
@@ -6,6 +7,19 @@ class FaceTarget:
     def __init__(self, label, proba, box):
         self.label = label
         self.proba = proba
-        self.box = box
+        self._box = box
+        self._lock = Lock()
         self.id = uuid.uuid4().hex
+
+    @property
+    def box(self):
+
+        with self._lock:
+            return self._box
+
+    @box.setter
+    def box(self, val):
+
+        with self._lock:
+            self._box = val
 
