f127db539eb75293bb23508acd26e6cc8aa389b3
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-10 15:15:50 +0800

    add ssd mobilenet object detector to downloaded models

--- bin/download_models.sh
+++ bin/download_models.sh
@@ -9,14 +9,14 @@ fi
 
 cd $model_path
 
-models=(
+dlib_models=(
     shape_predictor_68_face_landmarks.dat
     shape_predictor_5_face_landmarks.dat
     mmod_human_face_detector.dat
     dlib_face_recognition_resnet_model_v1.dat
 )
 
-for model in "${models[@]}"; do
+for model in "${dlib_models[@]}"; do
 
     if [ ! -e $model ]; then
         wget http://dlib.net/files/${model}.bz2
@@ -26,9 +26,16 @@ for model in "${models[@]}"; do
 
 done
 
-upper_body_detector=haarcascade_upperbody.xml
+object_detector=ssd_mobilenet_v1_coco_2017_11_17
 
-if [ ! -e $upper_body_detector ]; then
-    wget https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/${upper_body_detector}
+if [ ! -e $object_detector ]; then
+    mkdir $object_detector && cd $object_detector
+
+    wget https://raw.githubusercontent.com/opencv/opencv_extra/master/testdata/dnn/${object_detector}.pbtxt
+    wget http://download.tensorflow.org/object_detectors/object_detection/${object_detector}.tar.gz
+    tar -zxvf ${object_detector}.tar.gz && rm -f ${object_detector}.tar.gz
+    cp ${object_detector}/frozen_inference_graph.pb ${object_detector}.pb
+
+    rm -rf $object_detector
 fi
 
