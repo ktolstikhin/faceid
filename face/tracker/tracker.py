c1c0e6330f24df2f21212624aab7e97aebc1f3e8
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-10 18:01:00 +0800

    tune face tracker

--- face/tracker/tracker.py
+++ face/tracker/tracker.py
@@ -12,7 +12,7 @@ from .utils import box_center
 class FaceTracker:
 
     TARGET_LOST_FRAMES = 10
-    TARGET_LABEL_CANDIDATES = 20
+    TARGET_LABEL_CANDIDATES = 15
 
     def __init__(self, img_size):
         self.img_size = img_size
