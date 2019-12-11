abfac6da9342f5923e7cf7c2c52c51966744347f
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-11 10:55:20 +0800

    add batch prediction to people detector

--- people/detector.py
+++ people/detector.py
@@ -3,21 +3,35 @@ import os
 import cv2
 
 from utils.logger import init_logger
+from vision.predictor import Predictor
 
 
-class PopleDetector:
+class PeopleDetector(Predictor):
 
     DETECT_IMAGE_SIZE = (300, 300)
     DETECT_SCORE_THRES = 0.75
 
-    def __init__(self, model_path, log=None):
+    def __init__(self, log=None):
         self.log = log or init_logger('faceid')
+        self.load_models()
+
+    def load_models(self):
+        pwd = os.getcwd()
+        os.chdir(settings.model_conf_file.parent)
+
+        with open(settings.model_conf_file) as f:
+            cfg = json.load(f)
+
+        model_path = cfg['people_detector']
         self.log.info(f'Load a people detector from {model_path}')
+
         model_name = os.path.basename(model_path)
         self.detector = cv2.dnn.readNetFromTensorflow(
             os.path.join(model_path, model_name + '.pb'),
             os.path.join(model_path, model_name + '.pbtxt')
         )
+
+        os.chdir(pwd)
 
     def detect(self, images):
         image_blob = cv2.dnn.blobFromImages(images,
@@ -59,4 +73,17 @@ class PopleDetector:
             people_dets.append(person_boxes)
 
         return people_dets
+
+    def predict(self, images, batch_size=32):
+        batches = round(len(images) / batch_size) or 1
+        predictions = []
+
+        for i in range(batches):
+            batch_start = i * batch_size
+            batch_end = (i + 1) * batch_size
+            batch = images[batch_start:batch_end]
+            batch_preds = self.detect(batch)
+            predictions.append(batch_preds)
+
+        return np.concatenate(predictions)
 
