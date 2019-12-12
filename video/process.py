b3d3a8dbe6e500b1b8564bc1b2c00695e6d4a2f2
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-12-12 11:24:42 +0800

    add video stream process

---
+++ video/process.py
@@ -0,0 +1,52 @@
+import ctypes
+from multiprocessing import Process, Array, Event
+
+import numpy as np
+
+from .stream import VideoStream
+from utils.logger import init_logger
+
+
+class VideoStreamProcess(Process):
+
+    def __init__(self, path='/dev/video0', size=(640, 480), log=None):
+        self._stream = VideoStream(path, size)
+        self._join_event = Event()
+        self._log = log or init_logger('faceid')
+        w, h = size
+        self._frame_shape = (h, w, 3)
+        array_size = int(np.prod(self._frame_shape))
+        self._frame = Array(ctypes.c_int8, array_size)
+        super().__init__(name='VideoStream')
+
+    @property
+    def size(self):
+        return self._stream.size
+
+    @property
+    def path(self):
+        return self._stream.path
+
+    def _shared_to_numpy(self):
+        arr = np.frombuffer(self._frame.get_obj(), dtype='uint8')
+
+        return arr.reshape(self._frame_shape)
+
+    def read(self):
+
+        with self._frame.get_lock():
+            return self._shared_to_numpy()
+
+    def run(self):
+
+        while not self._join_event.is_set():
+            frame = self._stream.read()
+
+            with self._frame.get_lock():
+                arr = self._shared_to_numpy()
+                np.copyto(arr, frame)
+
+    def join(self, timeout=None):
+        self._join_event.set()
+        super().join(timeout)
+
